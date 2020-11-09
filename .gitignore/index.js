32798cff3781dd05ac5e4391aa930a0aa1c4e2bc
iPadrxz <74182288+iPadrxz@users.noreply.github.com>
2020-11-09 11:59:03 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,12 +1,13 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const { Client, Message } = require("discord.js");
+const { PREFIX } = require('./config');
+const client = new Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 }); 
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
+    if (msg.content === `${PREFIX}ping`) {
         msg.reply('Pong');
     }
 });
